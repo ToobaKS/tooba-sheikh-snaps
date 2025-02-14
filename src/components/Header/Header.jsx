0c4f03c1617b000f5e1aa3005fd5f27563d181c0
Tooba Sheikh <tksheikh17@gmail.com>
2025-02-14 10:03:10 -0500

    Finished styling the hero component for mobile, and the wordamrk in the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ import img from "../../assets/images/Filter.svg";
 function Header({ toggleFilterContainer }) {
   return (
     <header className="header">
-      <h1 className="header__heading">Snaps</h1>
+      <h1 className="header__wordmark">Snaps</h1>
       <button onClick={toggleFilterContainer} className="header__filter">Filter
-        <img src={img} alt="filter" />
+        <img className="header__filter-image" src={img} alt="filter" />
       </button>
     </header>
   );
