4134ab88c05968a7f61f586060a0d920eb434900
Tooba Sheikh <tksheikh17@gmail.com>
2025-02-16 14:45:45 -0500

    Fixed the author name on photograph styling

--- src/components/FilterContainer/FilterContainer.jsx
+++ src/components/FilterContainer/FilterContainer.jsx
@@ -2,12 +2,20 @@ import tags from "../../data/tags.json";
 import "./FilterContainer.scss";
 import Tags from "../Tags/Tags";
 
-function FilterContainer({handleFilterPhotos, selectedTag}){
-    return(
-        <section className="container">
-            <Tags handleFilterPhotos={handleFilterPhotos} tags={tags} isClickable={true} selectedTag={selectedTag} />
-        </section>
-    );
+function FilterContainer({ handleFilterPhotos, selectedTag }) {
+  return (
+    <section className="filter-container">
+      <h2 className="filter-container__heading">Filters</h2>
+      <div className="filter-container__tags">
+      <Tags
+        handleFilterPhotos={handleFilterPhotos}
+        tags={tags}
+        isClickable={true}
+        selectedTag={selectedTag}
+      />
+      </div>
+    </section>
+  );
 }
 
 export default FilterContainer;
